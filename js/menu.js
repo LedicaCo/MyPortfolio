99062ecd87bfeab8534f4d11cbeef6bcb3cd4afe
LedicaCo <webdesign@ledica.co>
2023-09-08 19:18:15 -0500

    Advances

--- js/menu.js
+++ js/menu.js
@@ -1,23 +1,3 @@
-/* const toggler = document.querySelector('.btn');
-toggler.addEventListener('click', function(){
-    document.querySelector('#sidebar').classList.toggle('collapsed');
-}); */
-
-/* document.addEventListener("DOMContentLoaded", function () {
-    const toggleButton = document.getElementById("toggleButton");
-    const verticalMenu = document.getElementById("verticalMenu");
-
-    toggleButton.addEventListener("click", function () {
-        if (verticalMenu.style.left === "-250px") {
-            // Muestra el menú
-            verticalMenu.style.left = "0";
-        } else {
-            // Oculta el menú
-            verticalMenu.style.left = "-250px";
-        }
-    });
-}); */
-
 document.addEventListener("DOMContentLoaded", function () {
     const toggleButton = document.getElementById("toggleButton");
     const verticalMenu = document.getElementById("verticalMenu");
@@ -30,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
             verticalMenu.style.left = "0";
             toggleButton.style.left = "260px"; // Mueve el botón a la izquierda cuando el menú se muestra
             menuIcon.classList.remove("bx-menu");
-            menuIcon.classList.add("bx-menu"); // Cambia el ícono a una "X" (o el ícono que prefieras)
+            menuIcon.classList.add("bx-menu"); // Cambia el ícono a una "X" (o el ícono que prefiera)
         } else {
             // Oculta el menú
             verticalMenu.style.left = "-250px";
